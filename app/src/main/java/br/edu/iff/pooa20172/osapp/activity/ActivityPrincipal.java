44eda2f8abd77efc2780977ae266f8c8d56de66c
lscon <lscon@users.noreply.github.com>
2017-12-11 17:28:26 -0200

    Criando splash screen (tela de abertura)

--- app/src/main/java/br/edu/iff/pooa20172/osapp/activity/ActivityPrincipal.java
+++ app/src/main/java/br/edu/iff/pooa20172/osapp/activity/ActivityPrincipal.java
@@ -1,7 +1,9 @@
-package br.edu.iff.pooa20172.osapp;
+package br.edu.iff.pooa20172.osapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.edu.iff.pooa20172.osapp.R;
 
 public class ActivityPrincipal extends AppCompatActivity {
 
