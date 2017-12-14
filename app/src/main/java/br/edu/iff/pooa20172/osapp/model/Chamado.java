01c0f735f700ad5d83ea393ee6ce1dbee57466aa
lscon <lscon@users.noreply.github.com>
2017-12-14 11:33:15 -0200

    Criando classe model Chamado

---
+++ app/src/main/java/br/edu/iff/pooa20172/osapp/model/Chamado.java
@@ -0,0 +1,40 @@
+package br.edu.iff.pooa20172.osapp.model;
+
+/**
+ * Created by fazenda on 14/12/17.
+ */
+
+public class Chamado {
+
+    private String descricao, usuario, dataRegistro;
+
+    public Chamado(String descricao, String usuario, String dataRegistro) {
+        this.descricao = descricao;
+        this.usuario = usuario;
+        this.dataRegistro = dataRegistro;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getDataRegistro() {
+        return dataRegistro;
+    }
+
+    public void setDataRegistro(String dataRegistro) {
+        this.dataRegistro = dataRegistro;
+    }
+}
