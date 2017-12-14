fffbe312bf498f67bc52e46a5c30e2466b08c4fa
lscon <lscon@users.noreply.github.com>
2017-12-14 09:20:36 -0200

    Editando AndroidManifest e styles

--- app/src/main/java/br/edu/iff/pooa20172/osapp/activity/ActivityAbertura.java
+++ app/src/main/java/br/edu/iff/pooa20172/osapp/activity/ActivityAbertura.java
@@ -1,5 +1,7 @@
 package br.edu.iff.pooa20172.osapp.activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,21 @@ import br.edu.iff.pooa20172.osapp.R;
 
 public class ActivityAbertura extends AppCompatActivity {
 
+    private static int ABERTURA_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abertura);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(ActivityAbertura.this, ActivityPrincipal.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, ABERTURA_TIME_OUT);
     }
 }
