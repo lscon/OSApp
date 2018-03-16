d5b314d6828c795c99117a468873ebdf083685b2
lscon <lscon@users.noreply.github.com>
2018-03-16 00:06:38 -0300

    Criando interface ClickRecyclerViewLister e editando adapter chamado

--- app/src/main/java/br/edu/iff/pooa20172/osapp/adapter/ChamadoAdapter.java
+++ app/src/main/java/br/edu/iff/pooa20172/osapp/adapter/ChamadoAdapter.java
@@ -2,6 +2,7 @@ package br.edu.iff.pooa20172.osapp.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import br.edu.iff.pooa20172.osapp.R;
 import br.edu.iff.pooa20172.osapp.model.Chamado;
@@ -17,31 +19,77 @@ import br.edu.iff.pooa20172.osapp.model.Chamado;
  * Created by fazenda on 14/12/17.
  */
 
-public class ChamadoAdapter extends ArrayAdapter<Chamado> {
+public class ChamadoAdapter extends RecyclerView.Adapter {
 
-    private final Context context;
-    private final ArrayList<Chamado> elementos;
+    private List<Chamado> chamados;
+    private Context context;
+    private static ClickRecyclerViewListener clickRecyclerViewListener;
 
-    public ChamadoAdapter(Context context, ArrayList<Chamado> elementos) {
-        super(context, R.layout.item_chamado, elementos);
+    public ChamadoAdapter(List<Chamado> chamados, Context context,ClickRecyclerViewListener clickRecyclerViewListener) {
+
+        this.chamados = chamados;
         this.context = context;
-        this.elementos = elementos;
+        this.clickRecyclerViewListener = clickRecyclerViewListener;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                      int viewType) {
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.item_chamado, parent, false);
+        ChamadoAdapter.ChamadoViewHolder chamadoViewHolder = new ChamadoAdapter.ChamadoViewHolder(view);
+        return chamadoViewHolder;
+    }
 
-        View rowView = inflater.inflate(R.layout.item_chamado, parent, false);
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder,
+                                 int position) {
 
-        TextView descricao = (TextView) rowView.findViewById(R.id.tvDescricao);
-        TextView setor = (TextView) rowView.findViewById(R.id.tvSetor);
-        TextView usuario = (TextView) rowView.findViewById(R.id.tvUsuario);
+        ChamadoAdapter.ChamadoViewHolder holder = (ChamadoAdapter.ChamadoViewHolder) viewHolder;
 
-        descricao.setText(elementos.get(position).getDescricao());
-        setor.setText(elementos.get(position).getSetor());
-        usuario.setText(elementos.get(position).getUsuario());
-        return rowView;
+        Chamado chamado = chamados.get(position) ;
+
+        holder.descricao.setText(Chamado.getDescricao());
+        holder.setor.setText(Chamado.getSetor());
+        holder.usuario.setText(Chamado.getUsuario());
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return chamados.size();
+    }
+
+    private void updateItem(int position) {
+
+    }
+
+    // Método responsável por remover um usuário da lista.
+    private void removerItem(int position) {
+
+    }
+
+    public class ChamadoViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView descricao;
+        private final TextView setor;
+        private final TextView usuario;
+
+
+        public ChamadoViewHolder(View itemView) {
+            super(itemView);
+            descricao = (TextView) itemView.findViewById(R.id.tvDescricao);
+            setor = (TextView) itemView.findViewById(R.id.tvSetor);
+            usuario = (TextView) itemView.findViewById(R.id.tvUsuario);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickRecyclerViewListener.onClick(chamados.get(getLayoutPosition()));
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/br/edu/iff/pooa20172/osapp/adapter/ClickRecyclerViewListener.java
@@ -0,0 +1,9 @@
+package br.edu.iff.pooa20172.osapp.adapter;
+
+/**
+ * Created by fazenda on 16/03/18.
+ */
+
+public interface ClickRecyclerViewListener {
+    void onClick(Object object);
+}
